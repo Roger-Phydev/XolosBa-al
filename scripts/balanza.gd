61736ed4aada6ebc0ab4c4dbbc506d338c700a6c
Daniel Casado <jdanielca@gmail.com>
2026-01-31 13:41:14 -0600

    Agrega colores a estatua.gd y agrega luz de exito

--- scripts/balanza.gd
+++ scripts/balanza.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+signal is_balanced;
+
 @export var peso_izquierda := 0.0;
 @export var peso_derecha := 0.0;
 @export var limites_angulos_deg := 10;
@@ -56,13 +58,26 @@ func tilt():
 		BALANCE_STATE.MIDDLE:
 			rotacion_objetivo = 0;
 	if (abs(rotacion_actual - rotacion_objetivo) < 0.1):
+		if (balance_state == BALANCE_STATE.MIDDLE):
+			exito();
 		return;
+		
+	fallo()
 	# Direccion deseada
 	var direccion : float = sign(rotacion_objetivo - rotacion_actual);
 	# Agregar velocidad de rotacion
 	var nueva_rotacion = rotacion_actual + direccion * rotational_speed * get_process_delta_time();
 	# Rotar
 	brazos.rotation_degrees.z = nueva_rotacion;
+	
+func exito():
+	is_balanced_done = true;
+	$LuzExito.visible = true;
+	is_balanced.emit()
+	
+func fallo():
+	is_balanced_done = false;
+	$LuzExito.visible = false;
 
 func this_print():
 	print({
--- scripts/estatua.gd
+++ scripts/estatua.gd
@@ -1,6 +1,13 @@
 extends CharacterBody3D
 
 var libre = true;# indica si el objeto está libre, osea, no agarrado
+@export var color : Color = Color.WHITE;
+
+func _ready() -> void:
+	var material = StandardMaterial3D.new();
+	material.albedo_color = color;
+	$model/Cabeza.set_surface_override_material(0,material);
+	$model/Cuerpo.set_surface_override_material(0,material);
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
