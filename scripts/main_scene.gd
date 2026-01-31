644f1bba6ca8d018cb18ab9d7c13c7caa5b11048
Daniel Casado <jdanielca@gmail.com>
2026-01-31 14:23:27 -0600

    Adds grey scale filter on Dog

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -15,6 +15,15 @@ func toogle_active_char():
 	GameMaster.man_active = not GameMaster.man_active; #toogles state of this variable
 	# then changes the state of active camera whether man is active or not
 	if GameMaster.man_active:
-		$Man.get_child(2).get_child(0).current = true;
+		var cameraMan = $Man.get_child(2).get_child(0)
+		cameraMan.current = true;
+		var cameraDog = $Dog.get_child(2).get_child(0);
+		var greyscaleLayer : CanvasLayer = cameraDog.get_node_or_null("CanvasLayer");
+		if (greyscaleLayer):
+			greyscaleLayer.hide();
 	else:
-		$Dog.get_child(2).get_child(0).current = true;
+		var camera = $Dog.get_child(2).get_child(0)
+		camera.current = true;
+		var greyscaleLayer : CanvasLayer = camera.get_node_or_null("CanvasLayer");
+		if (greyscaleLayer):
+			greyscaleLayer.show();
